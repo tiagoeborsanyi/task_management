ba78159b5be7204433e3188389dbfc786e9ad91b
Tiago Emerick <tiagoeborsanyi@gmail.com>
2022-11-25 11:32:53 -0300

    chore: format initial project

---
+++ lib/pages/presentations_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PresentationPage extends StatefulWidget {
+  const PresentationPage({super.key});
+
+  @override
+  State<PresentationPage> createState() => _PresentationPageState();
+}
+
+class _PresentationPageState extends State<PresentationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
